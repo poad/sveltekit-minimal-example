3680d37c5ff039fbffd2f640fb3f1189e8350041
Kenji Saito <ken-yo@mbr.nifty.com>
2026-02-20 08:36:46 +0900

    Bumps node_modules

---
+++ eslint.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'eslint/config';
+import {parser} from 'typescript-eslint';
+import eslintPluginSvelte from 'eslint-plugin-svelte';
+import * as svelteParser from 'svelte-eslint-parser';
+import svelteConfig from './svelte.config.js';
+
+import { includeIgnoreFile } from '@eslint/compat';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
+export default defineConfig(
+	includeIgnoreFile(gitignorePath),
+	{
+		ignores: ['node_modules/*', '.svelte-kit/*', 'eslint.config.js', 'svelte.config.js'],
+	},
+	...eslintPluginSvelte.configs['flat/recommended'],
+	{
+		plugins: {
+			svelte: eslintPluginSvelte,
+		},
+		languageOptions: {
+			parser: svelteParser,
+			parserOptions: {
+				parser,
+				extraFileExtensions: ['.svelte'],
+				svelteConfig,
+			},
+		},
+	},
+	{
+		ignores: ['node_modules/*', '.svelte-kit/*'],
+	},
+);
